93f1ffd3b6137d08a0d7706311e5bc5c438cc1e3
jessepeterman <jessepeterman@gmail.com>
2018-07-06 14:28:40 -0700

    cleaned up some unused code

--- docs/app.js
+++ docs/app.js
@@ -1,43 +1,29 @@
-const project1 = document.querySelector('.project-1');
-const project2 = document.querySelector('.project-2');
-const project4 = document.querySelector('.project-4');
-const project1Preview = document.querySelector('.project-1-preview');
-const aboutMeBtn = document.querySelector('.about-me-nav-button');
-const aboutMeDropDown = document.querySelector('.about-me');
-const navBtn = document.querySelector('.nav-btn');
-const navMenu = document.querySelector('.nav-menu');
-const projectsNavButton = document.querySelector('.projects-nav-button');
-const aboutMeNavButton = document.querySelector('.projects-nav-button');
-const contactInfoNavButton = document.querySelector('.projects-nav-button');
-const divider = document.querySelector('.menu-divider-bottom');
-const divider2 = document.querySelector('.menu-divider-top');
-const tooltip = document.querySelector('.tooltip');
+const project1 = document.querySelector(".project-1");
+const project2 = document.querySelector(".project-2");
+const project4 = document.querySelector(".project-4");
+const project1Preview = document.querySelector(".project-1-preview");
+const aboutMeBtn = document.querySelector(".about-me-nav-button");
+const aboutMeDropDown = document.querySelector(".about-me");
+const navBtn = document.querySelector(".nav-btn");
+const navMenu = document.querySelector(".nav-menu");
+const projectsNavButton = document.querySelector(".projects-nav-button");
+const aboutMeNavButton = document.querySelector(".projects-nav-button");
+const contactInfoNavButton = document.querySelector(".projects-nav-button");
+const divider = document.querySelector(".menu-divider-bottom");
+const divider2 = document.querySelector(".menu-divider-top");
+const tooltip = document.querySelector(".tooltip");
 
-navBtn.addEventListener('click', e => {
-  navMenu.classList.toggle('hidden');
-  divider.classList.toggle('hidden');
-  divider2.classList.toggle('hidden');
+navBtn.addEventListener("click", e => {
+  navMenu.classList.toggle("hidden");
+  divider.classList.toggle("hidden");
+  divider2.classList.toggle("hidden");
 });
 
 document.addEventListener(
-  'touchstart',
+  "touchstart",
   function addtouchclass(e) {
-    document.documentElement.classList.add('can-touch');
-    document.removeEventListener('touchstart', addtouchclass, false);
+    document.documentElement.classList.add("can-touch");
+    document.removeEventListener("touchstart", addtouchclass, false);
   },
   false
 );
-
-project2.addEventListener('mouseover', e => {
-  tooltip.classList.toggle('invisible');
-});
-
-project2.addEventListener('mouseout', e => {
-  tooltip.classList.toggle('invisible');
-});
-// project4.addEventListener('mouseover', e => {
-//   tooltip.classList.toggle('invisible');
-// });
-// project4.addEventListener('mouseout', e => {
-//   tooltip.classList.toggle('invisible');
-// });
